f727540f676fe06c3fe5ddcbfa55ffc5bbb4f9e0
Lucas Daniel Velazquez M <19197331+Luxter77@users.noreply.github.com>
2020-10-24 01:02:15 -0300

    stuff happens

--- hijackedrole/hijackedRole.py
+++ hijackedrole/hijackedRole.py
@@ -1,20 +1,100 @@
 #!/usr/bin/env python3
 # coding: utf-8
+from tqdm.asyncio import tqdm as asynctqdm
 from discord.ext import commands
+from tqdm.auto import tqdm
+import traceback
 import discord
-import random
+import asyncio
+import pickle
+import glob
 import sys
 import os
-
-CommandPrefix = '!role'
+import re
 
 
 # init
-bot = commands.Bot(command_prefix=CommandPrefix, case_insensitive=True)
-global slash
+bot = commands.Bot(command_prefix='!role', case_insensitive=True)
 slash = "\\" if (os.name == 'nt') else "/" # Where the heck am I
 
 #debug info
-global debugTrigger
+LogChan = 769403585262780416
 debugTrigger = False # It prints stuff
 
+## All of this was carried over from Hijacked Node
+async def logMe(st, err_ = False, tq = True):
+	if(err_):
+		_stderr = '|-----------------ERR_ START-------------------|'
+		print(_stderr) if(tq) else tqdm.write(_stderr)
+		print(st) if(tq) else tqdm.write(st)
+		try:
+			for Chan in LogChan:
+				await bot.get_channel(Chan).send('|-----------------ERR_ START-------------------|')
+				await bot.get_channel(Chan).send("<@" + str(LogAdmin[0]) + ">:")
+				await bot.get_channel(Chan).send(st)
+				await bot.get_channel(Chan).send('|------------------ERR_ END--------------------|')
+		except:
+			try:
+				for Chan in LogChan:
+					await bot.get_channel(Chan).send('|--------------- ERR_ START ----------------|')
+					try:
+						await bot.get_channel(Chan).send("<@" + str(LogAdmin[0]) + ">:")
+						await bot.get_channel(Chan).send(str(st))
+					except:
+						await bot.get_channel(Chan).send("<@" + str(LogAdmin[0]) + ">:")
+						await bot.get_channel(Chan).send("Some unprinteable error happened... ")
+					await bot.get_channel(Chan).send('|------------------ERR_ END--------------------|')
+			except:
+				_stderr = "Ah for hugs sake something went horribly grong! AGAIN"
+				print(_stderr) if(tq) else tqdm.write(_stderr)
+		_stderr = '|------------------ERR_ END--------------------|'
+		print(_stderr) if(tq) else tqdm.write(_stderr)
+	else:
+		_stdout = st
+		print(_stdout) if(tq) else tqdm.write(_stdout)
+		try:
+			for Chan in LogChan:
+				await bot.get_channel(Chan).send(st)
+
+		except:
+			try:
+				for Chan in LogChan:
+					try:
+						try:
+							await bot.get_channel(Chan).send(st)
+						except:
+							await bot.get_channel(Chan).send(str(st))
+					except:
+						await bot.get_channel(Chan).send("Some unprinteable error happened... ")
+			except:
+				await bot.get_channel(Chan).send('|------------------Log_ START------------------|')
+				await logMe("Ah for fucks sake something went horribly grong!", True)
+				await bot.get_channel(Chan).send('|------------------Log_ END--------------------|')
+
+
+@bot.event
+async def on_error(event_method, *args, **kwargs):
+	try:
+		await logMe(" ``` "	+	event_method			+	" ``` ", True)
+		await logMe(" ``` "	+	traceback.format_exc()	+	" ``` ", True)
+	except:
+		print('|------------------ERR_ START------------------|')
+		print(' Ignoring exception in {}'.format(event_method), file=sys.stderr)
+		traceback.print_exc()
+		print('|------------------ ERR_ END ------------------|')
+@bot.event
+async def on_ready():
+	await logMe('|-----------------doBootUp-st------------------|')
+	#await logMe('|            Testing Testing 1 2 3             |') #TODO: Think of a cool phrase to replace this one
+	await logMe('|----------------------------------------------|')
+	await bot.change_presence(activity = discord.Game(name = 'Waking Up...'))
+	async with bot.get_channel(LogChan).typing():
+		await logMe( "[ " + str(dt.datetime.now().timestamp()) + " ]" )
+		await logMe( str(bot.user) + " Is connected to:")
+		await logMe('|----------------------------------------------|')
+		for guild in bot.guilds:
+			await logMe(" - [" + str(guild.id) + "]: " + str(guild.name) + ".")
+		await logMe('|----------------------------------------------|')
+		await logMe("|			Bootup Sequence complete			|")
+		await bot.change_presence(activity = discord.Game(name = 'Soulcasting'))
+	await logMe('|-------------- doBootUp End ---------------|')
