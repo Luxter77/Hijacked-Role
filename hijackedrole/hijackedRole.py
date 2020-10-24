ee1565803321a092fa11119450901d3bab4c51a9
Lucas Daniel Velazquez M <19197331+Luxter77@users.noreply.github.com>
2020-10-24 00:48:54 -0300

    ææææææsdgafgagfnogoodtext

--- hijackedrole/hijackedRole.py
+++ hijackedrole/hijackedRole.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 # coding: utf-8
+from discord.ext import commands
 import discord
 import random
+import sys
+import os
+
+CommandPrefix = '!role'
+
+
+# init
+bot = commands.Bot(command_prefix=CommandPrefix, case_insensitive=True)
+global slash
+slash = "\\" if (os.name == 'nt') else "/" # Where the heck am I
+
+#debug info
+global debugTrigger
+debugTrigger = False # It prints stuff
+
